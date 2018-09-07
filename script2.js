cfaed12dd097a5619cbff9081bfe232327fd0e39
NedrNout <maxim.nedozrelov@ya.ru>
2018-09-08 01:27:33 +0300

    все 3 готовы

--- script2.js
+++ script2.js
@@ -14,7 +14,65 @@
 ** - В случае не прохождения валидации одним из полей необходимо выделять это поле красной рамкой
 * и сообщать пользователю об ошибке. */
 
+document.getElementById('submit').addEventListener('click', function () {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var message = document.getElementById('message').value;
+
+    if (/^\w+$/.test(message)) {
+        document.getElementById('message').className = ('form_inside');
+        document.getElementById('label_message').innerText = '';
+        console.log('Сообщение: ' + message + ' верное');
+    } else {
+        document.getElementById('message').classList.add('redBorder');
+        document.getElementById('label_message').innerText = 'Это поле не может быть пустым';
+    }
 
 
-var testRegexp = "I am 'your' friend, aren't I? Yes, you are.";
-console.log(testRegexp.replace(/\'(\w+)\'/g, '"$1"'));
+    if (/^[A-zА-яЁё]+$/.test(name)) {
+        document.getElementById('name').className = ('form_inside');
+        document.getElementById('label_name').innerText = '';
+        console.log('Имя: ' + name + ' верное');
+    } else if (name === "") {
+        document.getElementById('name').classList.add('redBorder');
+        document.getElementById('label_name').innerText = 'Это поле не может быть пустым';
+    } else {
+        document.getElementById('name').classList.add('redBorder');
+        document.getElementById('label_name').innerText = 'Имя должно состоять только из букв';
+        console.log('Имя: ' + name + ' Неверное');
+    }
+
+
+    if (/^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}$/.test(email)) {
+        document.getElementById('email').className = ('form_inside');
+        document.getElementById('label_email').innerText = '';
+        console.log('Эл. почта: ' + email + ' верное');
+    } else if (email === '') {
+        document.getElementById('email').classList.add('redBorder');
+        document.getElementById('label_email').innerText = 'Это поле не может быть пустым';
+    } else {
+        document.getElementById('email').classList.add('redBorder');
+        document.getElementById('label_email').innerText = 'Email указан неверно';
+        console.log('Эл. почта: ' + email + ' Неверная');
+    }
+
+
+    if (/^(8|\+7)(\(\d{3}\))(\d{3}-)(\d{4})$/.test(phone)) {
+        document.getElementById('phone').className = ('form_inside');
+        document.getElementById('label_phone').innerText = '';
+        console.log('Телефон: ' + phone + ' верное');
+    } else if (phone === '') {
+        document.getElementById('phone').classList.add('redBorder');
+        document.getElementById('label_phone').innerText = 'Это поле не может быть пустым';
+    } else {
+        document.getElementById('phone').classList.add('redBorder');
+        document.getElementById('label_phone').innerText = 'Телефон указан неверно';
+        console.log('Телефон: ' + phone + ' Неверное');
+    }
+
+    event.preventDefault();
+
+
+});
+
