9a9649cc5935f3449eb245199883cdcfa2cf84d6
NedrNout <maxim.nedozrelov@ya.ru>
2018-09-11 17:28:36 +0300

    исправил регулярки

--- script2.js
+++ script2.js
@@ -13,78 +13,82 @@
 *
 ** - В случае не прохождения валидации одним из полей необходимо выделять это поле красной рамкой
 * и сообщать пользователю об ошибке. */
+(function($){
+    $(function () {
+        document.getElementById('submit').addEventListener('click', function () {
+            var name = document.getElementById('name').value;
+            var email = document.getElementById('email').value;
+            var phone = document.getElementById('phone').value;
+            var city = document.getElementById('city').value;
+            var message = document.getElementById('message').value;
 
-document.getElementById('submit').addEventListener('click', function () {
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-    var city = document.getElementById('city').value;
-    var message = document.getElementById('message').value;
-
-    if (message) {
-        document.getElementById('message').className = ('form_inside');
-        document.getElementById('label_message').innerText = '';
-        console.log('Сообщение: ' + message + ' верное');
-    } else {
-        document.getElementById('message').classList.add('redBorder');
-        document.getElementById('label_message').innerText = 'Это поле не может быть пустым';
-    }
-
-
-    if (/(^[A-Z]{1}[a-z]{1,14} [A-Z]{1}[a-z]{1,14}$)|(^[А-Я]{1}[а-я]{1,14} [А-Я]{1}[а-я]{1,14}$)/.test(name)) {
-        document.getElementById('name').className = ('form_inside');
-        document.getElementById('label_name').innerText = '';
-        console.log('Имя: ' + name + ' верное');
-    } else if (name === "") {
-        document.getElementById('name').classList.add('redBorder');
-        document.getElementById('label_name').innerText = 'Это поле не может быть пустым';
-    } else {
-        document.getElementById('name').classList.add('redBorder');
-        document.getElementById('label_name').innerText = 'Имя должно состоять только из букв';
-        console.log('Имя: ' + name + ' Неверное');
-    }
-    if (city) {
-        document.getElementById('city').className = ('form_inside');
-        document.getElementById('label_city').innerText = '';
-        console.log('Город: ' + city + ' верное');
-    } else if (city === "") {
-        document.getElementById('city').classList.add('redBorder');
-        document.getElementById('label_city').innerText = 'Это поле не может быть пустым';
-    } else {
-        document.getElementById('city').classList.add('redBorder');
-        document.getElementById('label_city').innerText = 'Название города указано неверно';
-        console.log('Город: ' + city + ' Неверное');
-    }
-
-    if (/^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}$/.test(email)) {
-        document.getElementById('email').className = ('form_inside');
-        document.getElementById('label_email').innerText = '';
-        console.log('Эл. почта: ' + email + ' верное');
-    } else if (email === '') {
-        document.getElementById('email').classList.add('redBorder');
-        document.getElementById('label_email').innerText = 'Это поле не может быть пустым';
-    } else {
-        document.getElementById('email').classList.add('redBorder');
-        document.getElementById('label_email').innerText = 'Email указан неверно';
-        console.log('Эл. почта: ' + email + ' Неверная');
-    }
+            if (message) {
+                document.getElementById('message').className = ('form_inside');
+                document.getElementById('label_message').innerText = '';
+                console.log('Сообщение: ' + message + ' верное');
+            } else {
+                document.getElementById('message').classList.add('redBorder');
+                document.getElementById('label_message').innerText = 'Это поле не может быть пустым';
+            }
 
 
-    if (/^(8|\+7)(\(\d{3}\))(\d{3}-)(\d{4})$/.test(phone)) {
-        document.getElementById('phone').className = ('form_inside');
-        document.getElementById('label_phone').innerText = '';
-        console.log('Телефон: ' + phone + ' верное');
-    } else if (phone === '') {
-        document.getElementById('phone').classList.add('redBorder');
-        document.getElementById('label_phone').innerText = 'Это поле не может быть пустым';
-    } else {
-        document.getElementById('phone').classList.add('redBorder');
-        document.getElementById('label_phone').innerText = 'Телефон указан неверно';
-        console.log('Телефон: ' + phone + ' Неверное');
-    }
+            if (/(^\w+$)|(^\w+\s\w+$)|(^\w+\s\w+\s\w+$)/.test(name)) {
+                document.getElementById('name').className = ('form_inside name');
+                document.getElementById('label_name').innerText = '';
+                console.log('Имя: ' + name + ' верное');
+            } else if (name === "") {
+                document.getElementById('name').classList.add('redBorder');
+                document.getElementById('label_name').innerText = 'Это поле не может быть пустым';
+            } else {
+                document.getElementById('name').classList.add('redBorder');
+                document.getElementById('label_name').innerText = 'Имя должно состоять только из букв';
+                console.log('Имя: ' + name + ' Неверное');
+            }
+            if (city) {
+                document.getElementById('city').className = ('form_inside');
+                document.getElementById('label_city').innerText = '';
+                console.log('Город: ' + city + ' верное');
+            } else if (city === "") {
+                document.getElementById('city').classList.add('redBorder');
+                document.getElementById('label_city').innerText = 'Это поле не может быть пустым';
+            } else {
+                document.getElementById('city').classList.add('redBorder');
+                document.getElementById('label_city').innerText = 'Название города указано неверно';
+                console.log('Город: ' + city + ' Неверное');
+            }
 
-    event.preventDefault();
+            if (/^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}$/.test(email)) {
+                document.getElementById('email').className = ('form_inside');
+                document.getElementById('label_email').innerText = '';
+                console.log('Эл. почта: ' + email + ' верное');
+            } else if (email === '') {
+                document.getElementById('email').classList.add('redBorder');
+                document.getElementById('label_email').innerText = 'Это поле не может быть пустым';
+            } else {
+                document.getElementById('email').classList.add('redBorder');
+                document.getElementById('label_email').innerText = 'Email указан неверно';
+                console.log('Эл. почта: ' + email + ' Неверная');
+            }
 
 
-});
+            if (/^(8|\+7)(\(\d{3}\))(\d{3}-)(\d{4})$/.test(phone)) {
+                document.getElementById('phone').className = ('form_inside');
+                document.getElementById('label_phone').innerText = '';
+                console.log('Телефон: ' + phone + ' верное');
+            } else if (phone === '') {
+                document.getElementById('phone').classList.add('redBorder');
+                document.getElementById('label_phone').innerText = 'Это поле не может быть пустым';
+            } else {
+                document.getElementById('phone').classList.add('redBorder');
+                document.getElementById('label_phone').innerText = 'Телефон указан неверно';
+                console.log('Телефон: ' + phone + ' Неверное');
+            }
+
+            event.preventDefault();
+
+
+        });
+    })
+})(jQuery);
+
 
